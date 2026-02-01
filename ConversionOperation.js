a706db73b914039ccc0feea4ceaf6a1991bbc77c
Muhammad Zohaib <zohaibsaraj10@gmail.com>
2026-02-01 19:53:40 +0500

    CoversionOperations implemented

--- ConversionOperation.js
+++ ConversionOperation.js
@@ -4,3 +4,21 @@ let score = 33;
 console.log(typeof score)
 console.log(typeof (score))
 
+let valueInNumber = Number(score);
+
+console.log(typeof vauleInNumber);
+console.log(valueInNumber);
+
+
+//conversion
+
+// "33" => easily converted to 33 using line 7
+// "33abc" => not converted to number, rather a NaN
+// "null" => converted to zero
+// "undefined" => converted to NaN(not a number)
+// "zohaib" => converted to NaN
+
+
+let isLoggedIn = undefined;
+let booleanConversionIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanConversionIsLoggedIn);
